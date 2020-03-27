8f0ba76f25d22d4e8f60815ca49d2c6bef8409e4
Marissa <marissacooterl@gmail.com>
2020-03-27 10:40:43 -0500

    implemented use effect

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,38 +1,48 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import * as yup from "yup";
 import axios from "axios";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 //setting up my validations
 const formSchema = yup.object().shape({
-    name: yup.string().required("Name is a required field"),
-    pizzaSize: yup.string().required("Select a size"),
-//maybe change back to yup.string
-    toppings: yup.array().required("Please agree to terms of use"),
-//or try this: 
-// toppings: yup.string().required("sausage"),
-    specInstr: yup.string().required("Add special instructions for your delivery!")
-})
+  name: yup.string().required("Name is a required field"),
+  pizzaSize: yup.string().required("Select a size"),
+  //maybe change back to yup.string
+  toppings: yup.array().required("Please agree to terms of use"),
+  //or try this:
+  // toppings: yup.string().required("sausage"),
+  specInstr: yup
+    .string()
+    .required("Add special instructions for your delivery!")
+});
 
 //setting my state for form
 function Pizza() {
-    const [formState, setFormState] = useState({
-      name: "",
-      pizzaSize: "",
-      toppings: [],
-      specInstr: ""
-    });
-}
-
-//setting up state for errors
-const [errors, setErrors] = useState({
+  const [formState, setFormState] = useState({
+    name: "",
+    pizzaSize: "",
+    toppings: [],
+    specInstr: ""
+  });
+  //setting up state for errors
+  const [errors, setErrors] = useState({
     name: "",
     pizzaSize: "",
     toppings: "",
     specInstr: ""
   });
 
-//setting up state for disabled button until req fields are filled out
-const [buttonDisabled, setButtonDisabled] = useState(true);
+  //setting up state for disabled button until req fields are filled out
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  //validating use effect
+  useEffect(() => {
+    formSchema.isValid(formState).then(valid => {
+      setButtonDisabled(!valid);
+    });
+  }, [formState]);
+
+  
+}
 
 export default Pizza;
