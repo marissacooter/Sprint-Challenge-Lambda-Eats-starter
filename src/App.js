bf7af56a62cc53cf0b7c361fecee8dab7ca7780c
Marissa <marissacooterl@gmail.com>
2020-03-27 10:21:57 -0500

    created my routes for components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import Pizza from "./components/Pizza";
+import Homepage from "./components/Homepage";
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
+      {/* my routes are here */}
+      <div>
+        <Route exact path="/" component={Homepage}/>
+        <Route exact path="/pizza" component={Pizza}/>
+      </div>
     </>
   );
 };
