b8c59f9d20d85a5561b65b6fb7b593f82ef240a0
Marissa <marissacooterl@gmail.com>
2020-03-27 10:24:06 -0500

    created home page component frame

---
+++ src/components/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homepage = () => {
+    return (
+        <div>
+            put home page content here later
+        </div>
+    )
+}
+
+export default Homepage;
